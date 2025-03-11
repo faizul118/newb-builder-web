436a53eff6f853b81ea92022455897582c9fbedc
faizul118 <randommurgi@gmail.com>
2025-03-11 11:29:58 +0000

    nothing

---
+++ newb-builder-web.ps1
@@ -0,0 +1,9 @@
+switch (${{ github.event.inputs.platform }}) {
+    'Windows' { Write-Output "The platform is Windows" }
+    'Android' { Write-Output "The platform is Android" }
+    'IOS' { Write-Output "The platform is iOS" }
+    'Merged' { Write-Output "The platform is Merged" }
+    'All' { Write-Output "The platform is All" }
+    default { Write-Output "The platform is unknown" }
+}
+Write-Output $env:murgi World!
